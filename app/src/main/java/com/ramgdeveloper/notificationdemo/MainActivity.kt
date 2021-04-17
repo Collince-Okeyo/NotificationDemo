63ed5655be3aa4d8932288af1e1e57a7b7ce957a
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-04-17 21:25:54 +0300

    Created simple notification

--- app/src/main/java/com/ramgdeveloper/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/ramgdeveloper/notificationdemo/MainActivity.kt
@@ -7,18 +7,21 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btn: Button
     private val channelID = "com.ramgdeveloper.notificationdemo.channel1"
+    private val channelName = "MyChannel"
+    private val notificationId = 1
     private var notificationManager: NotificationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        creteNotificationChannel(channelID, "Notification Demo", "Simple notification demo")
+        creteNotificationChannel()
         btn = findViewById(R.id.button)
 
 
@@ -30,25 +33,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification(){
 
-        val notificationId = 1
-        val notification = NotificationCompat.Builder(this@MainActivity, channelID)
+        // Building Notification
+        val notification = NotificationCompat.Builder(this, channelID)
             .setContentTitle("Notification Demo")
             .setContentText("My simple notification demo")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .build()
-        notificationManager?.notify(notificationId,notification)
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.notify(notificationId,notification)
     }
 
-    private fun creteNotificationChannel(id: String, name: String, channelDescription: String){
+    private fun creteNotificationChannel(){
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
 
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(id, name, importance)
+            val channel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+
             notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
             notificationManager!!.createNotificationChannel(channel)
+
         }
 
     }
