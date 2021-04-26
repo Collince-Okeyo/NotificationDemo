b88c7cfb6d313049dfbfb087fb2e6f4cc3373692
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-04-26 23:33:20 +0300

    Added reply status

--- app/src/main/java/com/ramgdeveloper/notificationdemo/SecondActivity.kt
+++ app/src/main/java/com/ramgdeveloper/notificationdemo/SecondActivity.kt
@@ -1,10 +1,13 @@
 package com.ramgdeveloper.notificationdemo
 
+import android.app.NotificationManager
 import android.app.RemoteInput
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var text: TextView
@@ -24,6 +27,18 @@ class SecondActivity : AppCompatActivity() {
         if (remoteInput != null) {
             val inputString = remoteInput.getCharSequence(keyReply).toString()
             text.text = inputString
+
+            val channelID = "com.ramgdeveloper.notificationdemo.channel1"
+             val notificationId = 1
+
+            val repliedNotification = NotificationCompat.Builder(this, channelID)
+                    .setSmallIcon(android.R.drawable.ic_dialog_info)
+                    .setContentText("Replied Successfully")
+                    .build()
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(notificationId, repliedNotification)
+
         }
 
     }
